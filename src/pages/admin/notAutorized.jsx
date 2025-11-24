a98f8e3ee3f8c614b9cc10573a2d6dc0933c31a2
TomasManazza80 <tomas.manazza19@gmail.com>
2025-11-24 04:38:52 -0300

    ALL FRONT

--- src/pages/admin/notAutorized.jsx
+++
@@ -1,8 +0,0 @@
-const NotAuthorized = () => (
-    <div>
-        <h1>Acceso denegado</h1>
-        <p>No tienes permiso para ver esta página.</p>
-    </div>
-);
-
-export default NotAuthorized;
--- src/store/redux/ReduxStore.jsx
+++
@@ -1,5 +0,0 @@
-import { createStore } from "redux";
-
-import { CartReducer } from "./cart/CartReducer";
-
-export const ReduxStore = createStore(CartReducer);
